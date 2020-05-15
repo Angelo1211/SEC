ee860b9d4c466ddaa1974e4de7316e0bfe9b58fc
Angelo1211 <angelo12@vt.edu>
2020-05-15 06:09:48 +0100

    Changes to the way we access emacs config.

--- init.el
+++ init.el
@@ -14,20 +14,6 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-;;Load in the org mode file emacs source code, allows us to have
-(org-babel-load-file "w:/.emacs.d/configuration.org")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-	(flycheck xterm-color evil-magit magit org-bullets evil-org use-package evil auto-compile atom-one-dark-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;Load in the org mode file emacs source code, allows us to have
+(org-babel-load-file "~/.emacs.d/configuration.org")
