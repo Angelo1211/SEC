85b2191845c768b5fde77f7dcc24eafe7f87258b
Angel Ortiz <angelo12@vt.edu>
2021-12-29 15:41:49 +0000

    Rainbow delimeters

--- init.el
+++ init.el
@@ -17,3 +17,16 @@
 
 ;;Load in the org mode file emacs source code, allows us to have
 (org-babel-load-file "~/.emacs.d/configuration.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-delimiters racket-mode evil-magit magit evil-org evil org-bullets use-package auto-compile)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
